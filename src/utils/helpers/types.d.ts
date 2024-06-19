c3173036ad8b2b073cebf053c4c33a32b3fb5288
Long Duy <longduy@LongLDs-MacBook-Pro.local>
2024-06-19 19:22:04 +0700

    ci(admin): no_issue cicd scripts

--- src/utils/helpers/types.d.ts
+++ src/utils/helpers/types.d.ts
@@ -1,3 +1,3 @@
 type WithSelectors<S> = S extends { getState: () => infer T }
-    ? S & { use: { [K in keyof T]: () => T[K] } }
-    : never
+  ? S & { use: { [K in keyof T]: () => T[K] } }
+  : never;
