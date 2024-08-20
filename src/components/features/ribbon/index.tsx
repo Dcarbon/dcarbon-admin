48f530a71e1f7f021654c37cff38a49f8ce2a42a
vietld <vietld@fabbi.com.vn>
2024-08-20 17:49:23 +0700

    fix(admin): no_issue ui

---
+++ src/components/features/ribbon/index.tsx
@@ -0,0 +1,32 @@
+import { memo, useEffect, useState } from 'react';
+
+const Ribbon = memo(() => {
+  const [env, setEnv] = useState('');
+
+  useEffect(() => {
+    const hostname = window.location.hostname;
+
+    switch (hostname) {
+      case 'localhost':
+        setEnv('LOCAL');
+        break;
+      default:
+        if (import.meta.env.STAGE !== 'prod') {
+          setEnv(import.meta.env.STAGE.toUpperCase());
+        }
+        break;
+    }
+  }, []);
+
+  return (
+    <>
+      {env && (
+        <div className="ribbon-container">
+          <div className="ribbon-text">{env}</div>
+        </div>
+      )}
+    </>
+  );
+});
+
+export default Ribbon;
