71eb73b6bc90355d559ec565b3b91d54fabac991
August <longld@fabbi.com.vn>
2024-08-29 19:00:26 +0700

    refactor(admin): no_issue modify scp script

--- scripts/scp-env.sh
+++ scripts/scp-env.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 TYPE=
 SERVICE=
-while getopts "a:b:" opt
+OG=
+while getopts "a:b:c:" opt
 do
    # shellcheck disable=SC2220
    case "$opt" in
       a ) TYPE="$OPTARG" ;;
       b ) SERVICE="$OPTARG" ;;
+      c ) OG="$OPTARG" ;;
    esac
 done
-export $(egrep -v '^#' "$PWD/.env.deploy" | xargs)
+export $(egrep -v '^#' "$PWD/.env.$OG.deploy" | xargs)
 echo "$PEM_PATH"
 echo "$INSTANCE_IP"
 PWD=$PWD
 eval $(ssh-agent)
 ssh-add $PEM_PATH
-scp -i $PEM_PATH "$PWD/.env" "$INSTANCE_HOST:/my-app/env/${TYPE}/${SERVICE}/.env"
+scp -i $PEM_PATH "$PWD/.env.$OG.$TYPE" "$INSTANCE_HOST:/my-app/env/${TYPE}/${SERVICE}/.env"
